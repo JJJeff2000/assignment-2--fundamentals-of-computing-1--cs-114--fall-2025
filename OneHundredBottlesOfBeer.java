45b3f0e26e0290b931516d381b038459e28b6492
JJJeff2000 <jschrock09@gmail.com>
2025-09-25 15:27:47 -0400

    Now With NonVowel Counting

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,10 +1,13 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+// Use a for loop to count down from 100 to 0
     for (int i = 100; i > 0; i--) {
+// Print the lyrics using the i variable
       System.out.println(i + " bottles of beer on the wall");
       System.out.println(i + " bottles of beer");
       System.out.print("If one of those bottles");
       System.out.println(" should happen to fall");
+// Use the i variable to print the correct number of bottles left
       System.out.println(i - 1 + " bottles of beer on the wall");
       System.out.println(" ");
     }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-
+// Prompt the user to enter text
     System.out.print("Please type something here: ");
     String textInput = scanner.nextLine();
     System.out.println(" ");
     System.out.println("Reading...");
     System.out.println(" ");
-
+// Make the vowel counting veriables
     int aCount = 0;
     int eCount = 0;
     int iCount = 0;
     int oCount = 0;
     int uCount = 0;
-
+    int nonVowelCount = 0;
+// Count the vowels and non-vowels
     for (int i = 0; i < textInput.length(); i++) {
             switch (textInput.charAt(i)) {
               case 'a':
@@ -34,10 +35,15 @@ public class PrintVowelsAndNonVowels {
                 uCount++;
                 break;
               default:
+                nonVowelCount++;
                 break;
             }
         }
-
+// Print the results
+        System.out.println("Total number of non-vowels:");
+        System.out.println(" ");
+        System.out.println("Non-vowels: " + nonVowelCount);
+        System.out.println(" ");
         System.out.println("Total number of vowels:");
         System.out.println(" ");
         System.out.println("a: " + aCount);
@@ -45,7 +51,7 @@ public class PrintVowelsAndNonVowels {
         System.out.println("i: " + iCount);
         System.out.println("o: " + oCount);
         System.out.println("u: " + uCount);
-
+// Close the scanner
     scanner.close();
   }
 }
