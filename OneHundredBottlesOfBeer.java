cfc14d20f54b63ce0979ff8e9f9ad5c3b89fce03
JJJeff2000 <jschrock09@gmail.com>
2025-09-25 13:46:28 -0400

    Counting The Vowels

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -3,7 +3,8 @@ public class OneHundredBottlesOfBeer {
     for (int i = 100; i > 0; i--) {
       System.out.println(i + " bottles of beer on the wall");
       System.out.println(i + " bottles of beer");
-      System.out.println("If one of those bottles should happen to fall");
+      System.out.print("If one of those bottles");
+      System.out.println(" should happen to fall");
       System.out.println(i - 1 + " bottles of beer on the wall");
       System.out.println(" ");
     }
--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,51 @@
+import java.util.Scanner;
+
 public class PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Please type something here: ");
+    String textInput = scanner.nextLine();
+    System.out.println(" ");
+    System.out.println("Reading...");
+    System.out.println(" ");
+
+    int aCount = 0;
+    int eCount = 0;
+    int iCount = 0;
+    int oCount = 0;
+    int uCount = 0;
+
+    for (int i = 0; i < textInput.length(); i++) {
+            switch (textInput.charAt(i)) {
+              case 'a':
+                aCount++;
+                break;
+              case 'e':
+                eCount++;
+                break;
+              case 'i':
+                iCount++;
+                break;
+              case 'o':
+                oCount++;
+                break;
+              case 'u':
+                uCount++;
+                break;
+              default:
+                break;
+            }
+        }
+
+        System.out.println("Total number of vowels:");
+        System.out.println(" ");
+        System.out.println("a: " + aCount);
+        System.out.println("e: " + eCount);
+        System.out.println("i: " + iCount);
+        System.out.println("o: " + oCount);
+        System.out.println("u: " + uCount);
+
+    scanner.close();
   }
 }
