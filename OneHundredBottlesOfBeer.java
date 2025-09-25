9a6dca710e5fc3c84d3b20150aea6f2f870142d5
JJJeff2000 <jschrock09@gmail.com>
2025-09-25 13:08:51 -0400

    One Hundred Bottles of Beer

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -1,4 +1,11 @@
 public class OneHundredBottlesOfBeer {
   public static void main(String[] args) {
+    for (int i = 100; i > 0; i--) {
+      System.out.println(i + " bottles of beer on the wall");
+      System.out.println(i + " bottles of beer");
+      System.out.println("If one of those bottles should happen to fall");
+      System.out.println(i - 1 + " bottles of beer on the wall");
+      System.out.println(" ");
+    }
   }
 }
